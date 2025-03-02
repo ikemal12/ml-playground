dd9447dc6648db0174e08a713053b927ca5061a2
ikemal12 <iltar2005@gmail.com>
2025-03-02 23:01:16 +0000

    Preprocessing data

--- StockPredictor/stockpredictor.py
+++ StockPredictor/stockpredictor.py
@@ -23,4 +23,57 @@ if data_source == 'alphavantage':
             df = pd.DataFrame(columns=['Date','Low','High','Close','Open'])
             for k, v in data.items():
                 date = dt.datetime.strptime(k, '%Y-%m-%d')
-                data_row = [date.date(),float(v['3. low']),float(v['3. low']),] 
+                data_row = [date.date(),float(v['3. low']),float(v['2. high']),
+                            float(v['4. close']),float(v['1. open'])]
+                df.loc[-1,:] = data_row
+                df.index = df.index + 1
+        print('Data saved to : %s'%file_to_save)
+        df.to_csv(file_to_save)
+    else:
+        print('File already exists. Loading data from CSV')
+        df = pd.read_csv(file_to_save)
+else:
+    df = pd.read_csv(os.path.join('Stocks', 'hpq.us.txt'),delimiter=',',usecols=['Date','Open','High','Low','Close'])
+    print('Loaded data from the Kaggle repository') 
+
+df = df.sort_values('Date')
+print(df.head())
+
+plt.figure(figsize = (18,9))
+plt.plot(range(df.shape[0]),(df['Low']+df['High'])/2.0)
+plt.xticks(range(0,df.shape[0],500),df['Date'].loc[::500],rotation=45)
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Mid Price',fontsize=18)
+plt.show()
+
+# splitting and normalising data
+high_prices = df.loc[:,'High'].as_matrix()
+low_prices = df.loc[:,'Low'].as_matrix()
+mid_prices = (high_prices+low_prices)/2.0
+
+train_data = mid_prices[:11000]
+test_data = mid_prices[11000:]
+
+scaler = MinMaxScaler()
+train_data = train_data.reshape(-1,1)
+test_data = test_data.reshape(-1,1)
+
+smoothing_window_size = 2500
+for di in range(0,10000,smoothing_window_size):
+    scaler.fit(train_data[di:di+smoothing_window_size,:])
+    train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
+
+scaler.fit(train_data[di+smoothing_window_size:,:])
+train_data[di+smoothing_window_size:,:] = scaler.transform(train_data[di+smoothing_window_size:,:])
+
+train_data = train_data.reshape(-1)
+test_data = scaler.transform(test_data).reshape(-1)
+
+# exponential moving average smoothing
+EMA = 0.0
+gamma = 0.1
+for ti in range(11000):
+    EMA = gamma*train_data[ti] + (1-gamma)*EMA
+    train_data[ti] = EMA
+
+all_mid_data = np.concatenate([train_data,test_data],axis=0)
